d23a63b7eb94dbe4575d4e2bb0a04ceb313d0e6e
Peena Meghani <77peenameghani@gmail.com>
2025-08-22 20:33:04 +0530

    Python File
    Code Of Memory Location

---
+++ U1_P2.py
@@ -0,0 +1,7 @@
+#U1_P2 Write a program to display sum of two complex numbers. (TYBCA B-106 BHANDERI)
+
+complex_num1 = complex(input("Enter first number : "))
+complex_num2 = complex(input("Enter second number : "))
+
+print("Sum :", complex_num1 + complex_num2)
+
